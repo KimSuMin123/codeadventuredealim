5d690db1d746b3ba8c0f68a8cbc4f330b50bf27e
Kimsumin123 <tntn211@naver.com>
2024-06-22 14:47:02 +0900

    마이페이지 에러

--- src/pages/Language.js
+++ src/pages/Language.js
@@ -1,35 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Content, Title, Table, TableItem, Button, Links, Link, StatusContainer, StatusItem, CoinImage, StatusText, AdContainer } from '../style/LanguageStyle';
-import coin from '../img/coin.png';
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Content,
+  Title,
+  Table,
+  TableItem,
+  Button,
+  Links,
+  Link,
+  StatusContainer,
+  StatusItem,
+  CoinImage,
+  StatusText,
+  AdContainer,
+} from "../style/LanguageStyle";
+import coin from "../img/coin.png";
 
 function Language({ setMode, setSelectedLanguage }) {
-  const [userInfo, setUserInfo] = useState({ coin: 0, level: 1, experience: 0, nextLevelExp: 200 });
+  const [userInfo, setUserInfo] = useState({
+    coin: 0,
+    level: 1,
+    experience: 0,
+    nextLevelExp: 200,
+  });
 
   useEffect(() => {
-    fetch('/userinfo')
-      .then(res => res.json())
-      .then(data => {
+    fetch("/userinfo")
+      .then((res) => res.json())
+      .then((data) => {
         const { coin, experience, level } = data;
         let requiredExperience = 200;
         for (let i = 1; i < level; i++) {
           requiredExperience *= 2.5;
         }
-        setUserInfo({ coin, level, experience, nextLevelExp: requiredExperience });
+        setUserInfo({
+          coin,
+          level,
+          experience,
+          nextLevelExp: requiredExperience,
+        });
       })
-      .catch(err => console.error('Error fetching user info:', err));
+      .catch((err) => console.error("Error fetching user info:", err));
   }, []);
 
   const handleLanguageClick = (language) => {
     setSelectedLanguage(language);
-    setMode('STAGE');
+    setMode("STAGE");
   };
 
   useEffect(() => {
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.async = true;
-    script.type = 'text/javascript';
-    script.charset = 'utf-8';
-    script.src = 'https://t1.daumcdn.net/kas/static/ba.min.js';
+    script.type = "text/javascript";
+    script.charset = "utf-8";
+    script.src = "https://t1.daumcdn.net/kas/static/ba.min.js";
     document.body.appendChild(script);
   }, []);
 
@@ -47,7 +71,7 @@ function Language({ setMode, setSelectedLanguage }) {
       <Content>
         <Title>언어 선택</Title>
         <Table>
-          {['c', 'java', 'python', 'html', 'css', 'js'].map((language) => (
+          {["c", "java", "python", "html", "css", "js"].map((language) => (
             <TableItem key={language}>
               <Button onClick={() => handleLanguageClick(language)}>
                 {language.toUpperCase()}
@@ -56,8 +80,8 @@ function Language({ setMode, setSelectedLanguage }) {
           ))}
         </Table>
         <Links>
-          <Link href="#" onClick={() => setMode("MYPAGE")}>마이페이지</Link>
-          <Link href="#" onClick={() => setMode("SHOP")}>쇼핑</Link>
+          <Link onClick={() => setMode("MYPAGE")}>마이페이지</Link>
+          <Link onClick={() => setMode("SHOP")}>쇼핑</Link>
           <Link href="/logout">로그아웃</Link>
         </Links>
       </Content>
@@ -70,7 +94,9 @@ function Language({ setMode, setSelectedLanguage }) {
           <StatusText>Level: {userInfo.level}</StatusText>
         </StatusItem>
         <StatusItem>
-          <StatusText>EXP: {userInfo.experience}/{userInfo.nextLevelExp}</StatusText>
+          <StatusText>
+            EXP: {userInfo.experience}/{userInfo.nextLevelExp}
+          </StatusText>
         </StatusItem>
       </StatusContainer>
     </Container>
