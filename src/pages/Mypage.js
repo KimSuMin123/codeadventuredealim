83a43fdcfbd001b3c7bade5950bbca089f7a704b
Kimsumin123 <tntn211@naver.com>
2024-06-25 15:40:08 +0900

    55

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -38,7 +38,7 @@ function MyPage(props) {
         <UserInfoItem>HTML 단계: {userInfo.htmlst}</UserInfoItem>
         <UserInfoItem>CSS 단계: {userInfo.cssst}</UserInfoItem>
         <UserInfoItem>JS 단계: {userInfo.jsst}</UserInfoItem>
-        <img src="https://postfiles.pstatic.net/MjAyNDA2MjVfMTQw/MDAxNzE5Mjk2MjUzMTEz.ORhdpPv5VpGy9dWYP36xIldViCaoHILcQnYFf3XDFB0g.W-2imgaDPXyMYi9bsTAi9pbTwRHuqwJWxV_leoid4uUg.PNG/volcanomonster18.png?type=w773"></img>
+        <img src="https://steep-watchmaker-8f8.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F5486ac02-837a-4340-b853-a8cd7b03f65f%2F76d6c5a7-b6a0-4de6-a466-dda88f9bd14a%2FUntitled.png?table=block&id=5e3b79a1-2a07-4e25-834c-9b68804ef326&spaceId=5486ac02-837a-4340-b853-a8cd7b03f65f&width=2000&userId=&cache=v2"></img>
       </UserInfoContainer>
       <Button onClick={() => props.setMode("LANGUAGE")}>
         메인으로 돌아가기
