a9aca8e3119de7faf2f1d9c98513ff196792dcf8
Kimsumin123 <tntn211@naver.com>
2024-06-25 10:37:50 +0900

    up

--- src/style/stagestyle.js
+++ src/style/stagestyle.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Background = styled.div`
   background-image: url(${({ backgroundImage }) => backgroundImage});
@@ -15,12 +15,17 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: rgba(255, 255, 255, 0.5); /* 반투명 흰색 배경 */
- padding: 20px 220px;
+  background-color: rgba(
+    255,
+    255,
+    255,
+    0.5
+  ); /* Semi-transparent white background */
+  padding: 20px 220px;
 `;
 
 export const Title = styled.h2`
-  font-family: 'PixelEmulator', sans-serif;
+  font-family: "PixelEmulator", sans-serif;
   font-size: 24px;
   margin-bottom: 20px;
   text-align: center;
@@ -39,22 +44,26 @@ export const StageItem = styled.li`
 `;
 
 export const StageButton = styled.button`
-  font-family: 'Noto', sans-serif;
+  font-family: "Noto", sans-serif;
   font-size: 25px;
   padding: 10px;
   width: 60px; /* Adjust the width to make it a perfect circle */
   height: 60px; /* Adjust the height to make it a perfect circle */
   border: none;
-  border-radius: 30%; /* This makes the button round */
+  border-radius: 50%; /* This makes the button round */
   cursor: pointer;
   color: white;
-  background-color: ${({ completed }) => (completed ? 'GOLD' : 'SILVER')};
+  background-color: ${({ completed }) => (completed ? "GOLD" : "transparent")};
+  background-image: ${({ completed }) =>
+    completed ? "none" : "url(Lock.png)"};
+  background-size: cover;
+  background-position: center;
   &:disabled {
     cursor: not-allowed;
     background-color: gray;
+    background-image: none;
   }
 `;
-
 
 export const BackButton = styled.button`
   margin-top: 20px;
