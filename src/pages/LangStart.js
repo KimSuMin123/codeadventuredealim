7881fe3986a6dc78f4826ffd83aafc3a5d5605e2
Kimsumin123 <tntn211@naver.com>
2024-06-26 10:40:30 +0900

    u[\p

--- src/pages/LangStart.js
+++ src/pages/LangStart.js
@@ -1,44 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import styled from "styled-components";
 import devil from "../img/devil.png";
 import valla from "../img/valla/valla_idle_sw/1.png";
+import quizBackground from "../img/quiz.png";
+
+const CharacterContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 70px;
+`;
+
+const CharacterImage = styled.img`
+  width: 200px;
+  height: 300px;
+`;
+
+const DialogueBox = styled.div`
+  background: url(${quizBackground}) no-repeat center center;
+  background-size: cover;
+  width: 100%;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 20px;
+  box-sizing: border-box;
+  color: white;
+`;
 
 function LangStart() {
-  const [messages, setMessages] = useState({});
-  const [error, setError] = useState(null);
-  const lan = localStorage.getItem("selectedLanguage");
-
-  useEffect(() => {
-    if (!lan) {
-      setError("Language parameter is missing");
-      return;
-    }
-
-    fetch(`/get_message?lan=${lan}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => setMessages(data))
-      .catch((error) => {
-        console.error("Error fetching message:", error);
-        setError("Error loading messages");
-      });
-  }, [lan]);
-
   return (
     <div className="lang-start">
-      <div className="character-container">
-        <img src={valla} alt="valla" />
-        <img src={devil} alt="Devil" />
-      </div>
-      <div className="dialogue-box">
-        {error && <p>{error}</p>}
-        {!error && messages && Object.keys(messages).length > 0
-          ? Object.keys(messages).map((key) => <p key={key}>{messages[key]}</p>)
-          : !error && <p>Loading messages...</p>}
-      </div>
+      <CharacterContainer>
+        <CharacterImage src={valla} alt="valla" />
+        <CharacterImage src={devil} alt="Devil" />
+      </CharacterContainer>
+      <DialogueBox>
+        <p>소피아: 여긴 정말 깊고 어두워...</p>
+        <p>그레모리: 물속에는 예상치 못한 몬스터가 숨어있을 거야, 조심해.</p>
+        <p>소피아: 알겠어.</p>
+        <p>(갑자기 심해어가 나타났다!!)</p>
+        <p>그레모리: 저기 무언가 나타났어!</p>
+        <p>소피아: 전투 준비!</p>
+      </DialogueBox>
     </div>
   );
 }
